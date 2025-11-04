1592d989153a3f5fa465f28018ace0fad2991683
SeshuYaswanth <seshuyaswanthreddy01@gmail.com>
2025-11-04 09:50:15 +0530

    Memo

--- hooks/src/components/Memo.jsx
+++ hooks/src/components/Memo.jsx
@@ -4,21 +4,20 @@ import { useMemo, useState } from "react";
 import { intialItems } from "./items";
 
 const Memo = () => {
-    const [count, setCount] = useState(0);
-    const [items] = useState(intialItems);
+  const [count, setCount] = useState(0);
+  const [items] = useState(intialItems);
 
-    const findItems = useMemo(() => {
-        return items.find((item) => item.isSelected)
-    }, [items, count])
+  const findItems = useMemo(() => {
+    return items.find((item) => item.isSelected);
+  }, [items]);
 
-
-    return (
-        <div>
-            <h1>Count: {count}</h1>
-            <h1>Selected Item: {findItems?.id}</h1>
-            <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <h1>Selected Item: {findItems?.id}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+    </div>
+  );
+};
 
 export default Memo;
