36adf917e37b997a3ee51c7a94c9ef909c44b1a3
SeshuYaswanth <seshuyaswanthreddy01@gmail.com>
2025-11-07 09:18:27 +0530

    home

--- react-queries/src/App.jsx
+++ react-queries/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PostListing from "./components/PostListing";
 import "./app.css";
 import PostDetail from "./components/PostDetail";
+import Home from "./components/Home";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -10,6 +11,7 @@ const App = () => {
     <div>
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/posts" element={<PostListing />} />
           <Route path="/posts/:id" element={<PostDetail />} />
         </Routes>
